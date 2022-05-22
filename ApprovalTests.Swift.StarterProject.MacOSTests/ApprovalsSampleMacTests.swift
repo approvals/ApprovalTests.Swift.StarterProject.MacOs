d716f067dd707f4d8f4172f5ed56fa4c0a61792b
Jon Reid <jon@qualitycoding.org>
2022-05-22 15:27:39 -0700

    a Remove semicolon

--- ApprovalTests.Swift.StarterProject.MacOSTests/ApprovalsSampleMacTests.swift
+++ ApprovalTests.Swift.StarterProject.MacOSTests/ApprovalsSampleMacTests.swift
@@ -3,15 +3,13 @@ import ApprovalTests_Swift
 import XCTest
 
 class ApprovalsSampleMacTests: XCTestCase {
-
     func testList() throws {
         var names: [String] = ["Llewellyn", "James", "Dan", "Jason", "Katrina"]
         names.sort()
-        try Approvals.verifyAll(names);
+        try Approvals.verifyAll(names)
     }
-    
+
     func testText() throws {
         try Approvals.verify(UnitTests.getBenefits())
     }
-
 }
